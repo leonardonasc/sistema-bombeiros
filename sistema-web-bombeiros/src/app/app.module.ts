bbcb6e19801a42355272b2c66f92fc746fcefd3a
schmidtleonardo <schmidtleonard1@gmail.com>
2021-10-21 18:25:01 -0300

    Finaliza responsividade
    Finaliza responsividade na tela de usuário, menu lateral e dashboard.
    
    SWB-29 SWB-30 SWB-31 SWB-35

--- sistema-web-bombeiros/src/app/app.module.ts
+++ sistema-web-bombeiros/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TelaCadastroComponent } from './tela-cadastro/tela-cadastro.component';
--- sistema-web-bombeiros/src/app/componente-menu/componente-menu.component.ts
+++ sistema-web-bombeiros/src/app/componente-menu/componente-menu.component.ts
@@ -1,30 +1,52 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-componente-menu',
   templateUrl: './componente-menu.component.html',
-  styleUrls: ['./componente-menu.component.scss']
+  styleUrls: ['./componente-menu.component.scss'],
+  animations: [
+    trigger('slideIn', [
+      state(
+        'in',
+        style({
+          transform: 'translateX(-200px)',
+        })
+      ),
+      state(
+        'out',
+        style({
+          transform: 'translateX(0px)',
+        })
+      ),
+      transition('in => out', animate('350ms ease-in')),
+      transition('out => in', animate('350ms ease-in')),
+    ]),
+  ],
 })
 export class ComponenteMenuComponent implements OnInit {
-  classeMobile: String = "none";
-  classeDesktop: String = "";
-  classeBotao: String = "";
+  classeMobile: String = 'none';
+  classeDesktop: String = '';
+  classeBotao: String = '';
   windowWidth = window.innerWidth;
+  slideInState = 'in';
+  isOpen = true;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    if(this.windowWidth <= 540) {
-      this.classeBotao = "absolute";
-      this.classeDesktop = "none";
-    } else {
-      this.classeBotao = "none";
-      this.classeDesktop = "initial";
+    if (this.windowWidth > 670){
+      this.isOpen = false;
     }
   }
 
   abrirMenu() {
-
+    this.isOpen = !this.isOpen;
   }
-
 }
